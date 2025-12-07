108e7d87e3e524b3531d9349bce2a814f1e97906
Gonzalo A Flores <107579754+gflo1997@users.noreply.github.com>
2025-12-07 13:19:54 -0500

    Update __init__.py

--- HttpVisitor/__init__.py
+++ HttpVisitor/__init__.py
@@ -3,15 +3,13 @@ import azure.functions as func
 import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('HTTP trigger function processed a request.')
 
-    response_data = {
-        "count": 100,  # Test value
-        "message": "Step 6 Deployment Successful!"
-    }
+    # for now, return a test count
+    count = 1
 
     return func.HttpResponse(
-        json.dumps(response_data),
+        json.dumps({"count": count}),
         mimetype="application/json",
         status_code=200
     )
